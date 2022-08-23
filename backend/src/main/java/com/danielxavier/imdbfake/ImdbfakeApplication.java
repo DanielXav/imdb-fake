340c187eab1c2bdd4e64f06994194a1c6694d08f
Daniel Xavier <dxavier194@gmail.com>
2022-08-23 15:13:13 -0300

    First commit

---
+++ backend/src/main/java/com/danielxavier/imdbfake/ImdbfakeApplication.java
@@ -0,0 +1,13 @@
+package com.danielxavier.imdbfake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbfakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbfakeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/danielxavier/imdbfake/ImdbfakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielxavier.imdbfake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbfakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
