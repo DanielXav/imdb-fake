2d097fbad6bc5b681d1e8c5b133991b830974c8f
Daniel Xavier <dxavier194@gmail.com>
2022-08-24 11:59:52 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,7 @@
+function Form(){
+    return (
+        <h1>Formando</h1>
+    );
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,7 @@
+function Listing(){
+    return (
+        <h1>Listando</h1>
+    );
+}
+
+export default Listing;
